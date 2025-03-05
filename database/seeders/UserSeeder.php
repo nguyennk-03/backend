5fe581cae49a42d71c25a833e563a84268781ea5
Nguyen <152572528+nguyennk-03@users.noreply.github.com>
2025-03-05 19:55:46 +0700

    UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 use Faker\Factory as Faker;
 
@@ -16,22 +17,26 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'phone' => '0123456789',
             'avatar' => 'images/users/user-1.jpg',
             'address' => $faker->address,
             'role' => 'admin',
+            'remember_token' => Str::random(10),
         ]);
 
         for ($i = 0; $i < 10; $i++) {
             User::create([
                 'name' => $faker->name,
                 'email' => $faker->unique()->safeEmail,
+                'email_verified_at' => now(),
                 'password' => Hash::make('password'),
                 'phone' => $faker->phoneNumber,
                 'avatar' => User::getRandomImage(),
                 'address' => $faker->address,
                 'role' => 'user',
+                'remember_token' => Str::random(10),
             ]);
         }
     }
