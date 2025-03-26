1d4b680f63d93c275063ff66d945445e0a2f1b8f
Nguyen <152572528+nguyennk-03@users.noreply.github.com>
2025-03-26 08:30:27 +0700

    home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\ProductVariant;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $brands = Brand::all();
+
+        $categories = Category::whereNull('parent_id')->get();
+
+        $bestsellers = Product::with(['variants.images', 'brand'])
+            ->limit(8)
+            ->get();
+
+        $specialProducts = Product::with(['variants.images'])
+            ->where('brand_id', 4)
+            ->limit(4)
+            ->get();
+
+        return view('home', compact('brands', 'categories', 'bestsellers', 'specialProducts'));
     }
 }
