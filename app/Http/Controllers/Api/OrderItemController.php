025068e7ec9964f46cf51bb766ed63262bda0dde
Nguyen <152572528+nguyennk-03@users.noreply.github.com>
2025-05-06 13:20:03 +0700

    fix : update image for Nike Air Max 270 OrderItemController index

--- app/Http/Controllers/Api/OrderItemController.php
+++ app/Http/Controllers/Api/OrderItemController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\OrderItem;
 use App\Models\Order;
 use App\Models\Product;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -14,17 +13,7 @@ class OrderItemController extends Controller
 {
     public function index()
     {
-        $orderItems = OrderItem::with([
-            'product:id,name,stock_quantity,sold',
-            'order:id,code,status,total_price' // thêm dòng này
-        ])
-            ->whereHas('order', function ($query) {
-                $query->where('user_id', Auth::id());
-            })
-            ->select('id', 'order_id', 'product_id', 'quantity', 'price','image','size','color')
-            ->latest()
-            ->get();
-
+        $orderItems = OrderItem::with('product')->get();
         return response()->json($orderItems, Response::HTTP_OK);
     }
 
